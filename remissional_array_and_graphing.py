3f1a33e09046a6d971da6d3d27824bf0e828358b
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-02-27 20:31:20 +0000

    Complete remissional array and_postremissional graph

---
+++ remissional_array_and_graphing.py
@@ -0,0 +1,29 @@
+# This program is designed to use The Limit Definition of The Difference Quotient as for tangents in order to track whether or not cancer cells are actually moving and staying in remission by their adherence to The Electric Feather's array vs array_two curve.
+
+## It should also take blood work and graph the change of the present cancer cell data previous initial testing data. This curve can be compared to versus the electric feather's curve graph (array vs. array_two). This curve is dependent on the definition of The Unit Circle. 
+
+### The reason that this matters is that The Unit Circle for allows for standardization of trigonometric functions (sin(x), cos(x), tan(x), sincos(x), sec**2(x)) with a shape that crucial geometric regularity (such as a square, regular pentagon, and even the circle). This is useful to mapping changes in the geometry of the affected organ(s), possible tumors growths (or happily tumor remission), as well as unexpected, but treatable, metasicized findings. 
+
+import math;
+import cmath;
+from sympy import Symbol, integrate, sin, exp;
+
+### Given the fact that certain devices, as well as programming languages, have a limit to the precision of a number's decimal places, then by Information Theory and Planck-Time, certain microstates require more information and are more of a certainty. Essentially, if it is information-packed, then it is also more certain. As you can see with pi below, only a limited number of significant figures can be displayed, producing an information upper bound.
+pi = math.pi;
+print(pi);
+print("The fact that certain devices, as well as programming languages, have a limit to the precision of numerical decimal places, then by Information Theory and Planck-Time, certain microstates require more information and are moe of a certainty. Essentially, if it is information-packed, then it is also more certain. As you can see with pi below only a limited number of significant figures can be displayed, producing an information upper bound.");
+
+i = cmath.sqrt(-1);
+print("Calculated Value of i: ", i);
+
+e = i**2;
+print("Calculated Value of e: ", e);
+
+array = [(1/2), 1.00, 2.00, 3.00, e, i];
+print("This is to be known as The Unitary e Array.");
+print(array);
+
+#### Use the second array to plot up to six blood components against array, which is an exploration of The Information-Entropy Theory. Essentially, if array is confirmed, certain microstates are have lower probabilities and therefore are more spontaneous. If array was not spontaneous, it would have a higher probability of actualization.
+example_array_two = [(1/2), i**2, 2**(i**2), pi, cmath.sqrt((3*(i**2))), i]
+print(example_array_two);
+print("Use the second array to plot up to six blood components against array, which is an exploration of The Information-Entropy Theory. Essentially, if array is confirmed, certain microstates are have lower probabilities and therefore are more spontaneous. If array was not spontaneous, it would have a higher probability of actualizationion.")
