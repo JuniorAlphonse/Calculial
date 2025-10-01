10f0af8247191b0783f155662639313c6b720015
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-10-01 12:48:13 -0400

    Contemplating CancerorNah.py

---
+++ CancerorNah.py
@@ -0,0 +1,23 @@
+import math
+import cmath
+import matplotlib
+
+from matplotlib import pyplot
+phononenergy = [0, 50, 80, 90, 100];
+for energy in phononenergy:
+    print(energy * (.600000000000000000000000000000000000))
+x = (1)*(phononenergy[3]);
+y = (2 * x);
+
+def O(x, y):
+    if x != phononenergy[4]:
+        print(x, y);
+        
+O(x, y);
+print("The Mitocic regeneration of of the patient cells are stable.");
+
+pyplot.plot(x, y);
+pyplot.title("Opeation Module: Matter vs Time" );
+pyplot.xlabel( "Mitocic Matter Stuffing (M(pi))");
+pyplot.ylabel("Limited Diffential Timing (dt)");
+pyplot.show();
